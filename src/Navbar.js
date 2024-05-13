e2cb5ebc076c0da251e50541bdb3ec0367291d9a
panglew <pangle.weston@gmail.com>
2024-05-13 17:47:25 -0400

    Added js app

--- src/Navbar.js
+++ src/Navbar.js
@@ -2,21 +2,19 @@ import { Link, useMatch, useResolvedPath } from "react-router-dom"
 
 export default function Navbar() {
 
-    const path = window.location.pathname
-
     return <nav className="nav">
         <Link to="/" className="site-title"> Home Page </Link>
 
         <ul>
 
             <CustomLink to="/projects"> Projects </CustomLink>
-            <CustomLink href="/about"> About </CustomLink>
+            <CustomLink to="/about"> About </CustomLink>
 
         </ul>
     </nav>
 }
 
-function CustomLink({ href, children, ...props}) {
+function CustomLink({ to, children, ...props}) {
 
     const resolvedPath = useResolvedPath(to)
     const isActive = useMatch( {path: resolvedPath.pathname, end: true} )
