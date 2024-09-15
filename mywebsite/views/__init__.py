5157516d10749c91c795bd51874bb619fa62da0b
panglew <pangle.weston@gmail.com>
2024-09-14 23:06:33 -0400

    Correctly have index working. It's not pretty but will fix later

--- mywebsite/views/__init__.py
+++ mywebsite/views/__init__.py
@@ -1,4 +1,5 @@
 """Views, one for each my website page."""
-import mywebsite.views.home
+import mywebsite.views.about
+import mywebsite.views.index
 import mywebsite.views.projects
 import mywebsite.views.fragrances
