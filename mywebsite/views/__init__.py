b68f0b8dde7b27d02049564d742283788ef00cc6
panglew <pangle.weston@gmail.com>
2024-09-19 10:12:30 -0400

    Cleaning up files

--- mywebsite/views/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Views, one for each my website page."""
-import mywebsite.views.about
-import mywebsite.views.index
-import mywebsite.views.projects
-import mywebsite.views.fragrances
-import mywebsite.views.classes
--- mywebsite/views/fragrances.py
+++
@@ -1,9 +0,0 @@
-"""File to display fragrances.py"""
-import flask
-import mywebsite
-import mywebsite.model
-
-@mywebsite.app.route('/fragrances/')
-def show_fragrances():
-    """Function for fragrances."""
-    return flask.render_template('fragrance.html')
