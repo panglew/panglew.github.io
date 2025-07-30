14729aaa6ed3fe4fa5e6402b42d746a07750925d
panglew <pangle.weston@gmail.com>
2025-07-30 18:00:35 -0400

    Updating EECS494 page

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -11,6 +11,16 @@ export default async function Page({ params }: DynamicPageParams) {
 export function generateStaticParams() {
   return [
     { slug: "pick-it-up" },
+    { slug: "eecs494" },
+    { slug: "eecs491" },
+    { slug: "eecs489" },
+    { slug: "eecs485" },
+    { slug: "eecs484" },
+    { slug: "eecs482" },
+    { slug: "eecs388" },
+    { slug: "eecs370" },
+    { slug: "eecs281" },
+    { slug: "eecs280" },
   ];
 }
 
--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -20,5 +20,65 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: "never",
       priority: 0.5,
     },
+    {
+      url: "https://panglew.github.io/posts/eecs494",
+      lastModified: new Date(2024, 12, 15),
+      changeFrequency: "never",
+      priority: 0.5,
+    },
+    {
+      url: "https://panglew.github.io/posts/eecs491",
+      lastModified: new Date(2024, 12, 15),
+      changeFrequency: "never",
+      priority: 0.5,
+    },
+    {
+      url: "https://panglew.github.io/posts/eecs489",
+      lastModified: new Date(2024, 12, 15),
+      changeFrequency: "never",
+      priority: 0.5,
+    },
+    {
+      url: "https://panglew.github.io/posts/eecs485",
+      lastModified: new Date(2024, 12, 15),
+      changeFrequency: "never",
+      priority: 0.5,
+    },
+    {
+      url: "https://panglew.github.io/posts/eecs484",
+      lastModified: new Date(2024, 12, 15),
+      changeFrequency: "never",
+      priority: 0.5,
+    },
+    {
+      url: "https://panglew.github.io/posts/eecs482",
+      lastModified: new Date(2024, 12, 15),
+      changeFrequency: "never",
+      priority: 0.5,
+    },
+    {
+      url: "https://panglew.github.io/posts/eecs388",
+      lastModified: new Date(2024, 12, 15),
+      changeFrequency: "never",
+      priority: 0.5,
+    },
+    {
+      url: "https://panglew.github.io/posts/eecs370",
+      lastModified: new Date(2024, 12, 15),
+      changeFrequency: "never",
+      priority: 0.5,
+    },
+    {
+      url: "https://panglew.github.io/posts/eecs281",
+      lastModified: new Date(2024, 12, 15),
+      changeFrequency: "never",
+      priority: 0.5,
+    },
+    {
+      url: "https://panglew.github.io/posts/eecs280",
+      lastModified: new Date(2024, 12, 15),
+      changeFrequency: "never",
+      priority: 0.5,
+    },
   ];
 }
